94e003ba9dfdf562215ee3e2c238fcc806ab938c
daskrys <caperezl@ucsc.edu>
2023-10-08 16:25:49 -0700

    step 3 complete

--- src/counter.ts
+++ src/counter.ts
@@ -1,24 +1,11 @@
-/*
 export function setupCounter(element: HTMLButtonElement) {
-    let counter = 0
-    const setCounter = (count: number) => {
-      counter = count
-      element.innerHTML = `Christian's count is ${counter}`
-    }
-    element.addEventListener('click', () => setCounter(counter + 1))
-    setCounter(0)
-  }
-  */
+  let counter = 0;
 
-export function setupCounter(element: HTMLButtonElement) 
-{
-    let counter = 0;
-
-    let setCounter = (count: number) => {
-        counter = count;
-        element.innerHTML = `👻 Count: ${counter}`
-    }
-
-    element.addEventListener('click', () => setCounter(counter + 1))
-    setCounter(0)
+  const setCounter = (count: number) => {
+    counter = count;
+    element.innerHTML = `👻 Count: ${counter}`;
+  };
+  setInterval(() => setCounter(counter + 1), 1000);
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import "./style.css";
-import { setupCounter } from './counter.ts'
+import { setupCounter } from "./counter.ts";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
@@ -15,4 +15,4 @@ const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#btn')!)
+setupCounter(document.querySelector<HTMLButtonElement>("#btn")!);

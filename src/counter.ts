1b5749ceca665c5628f674b038bcc0f96464953b
daskrys <caperezl@ucsc.edu>
2023-10-11 09:17:59 -0700

    step 4 complete

--- src/counter.ts
+++ src/counter.ts
@@ -1,11 +1,23 @@
 export function setupCounter(element: HTMLButtonElement) {
   let counter = 0;
-
+/*
   const setCounter = (count: number) => {
     counter = count;
     element.innerHTML = `👻 Count: ${counter}`;
   };
-  setInterval(() => setCounter(counter + 1), 1000);
+  //setInterval(() => setCounter(counter + 1), 1000);
   element.addEventListener("click", () => setCounter(counter + 1));
   setCounter(0);
+*/
+  function animate() {
+
+    requestAnimationFrame(animate);
+
+    element.innerHTML = `👻 Count: ${counter++}`;
+  }
+
+  element.addEventListener("click", () => counter++);
+  window.requestAnimationFrame(animate);
 }
+
+

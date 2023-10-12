f5a43a8f6f865c1cc60a0549c10033e820257a61
daskrys <caperezl@ucsc.edu>
2023-10-12 11:38:43 -0700

    step 9 complete

--- src/counter.ts
+++ src/counter.ts
@@ -1,10 +1,21 @@
+interface Item {
+  name: string;
+  price: number;
+  rate: number;
+}
+
+const availableItems: Item[] = [
+  { name: "Ghost", price: 1000, rate: 1.15 },
+  { name: "Zombie", price: 10, rate: 1.15 },
+];
+
 export function setupCounter(element: HTMLButtonElement) {
   let counter = 0;
+
   let ghostCounter: number = 0;
   let zombieCounter: number = 0;
   let pumpCount: number = 0;
-  let ghostPrice: number = 1000; // the pricing for this doesn't make sense except to me
-  let zombiePrice: number = 10;
+
   const ghostButton = document.querySelector<HTMLButtonElement>("#btn2")!;
   const zombieButton = document.querySelector<HTMLButtonElement>("#btn3")!;
   const ghostLabel =
@@ -15,13 +26,13 @@ export function setupCounter(element: HTMLButtonElement) {
   function animate() {
     counter++;
 
-    if (counter >= ghostPrice) {
+    if (counter >= availableItems[0].price) {
       ghostButton.disabled = false;
     } else {
       ghostButton.disabled = true;
     }
 
-    if (ghostCounter >= zombiePrice) {
+    if (ghostCounter >= availableItems[1].price) {
       zombieButton.disabled = false;
     } else {
       zombieButton.disabled = true;
@@ -40,18 +51,20 @@ export function setupCounter(element: HTMLButtonElement) {
   });
 
   ghostButton.addEventListener("click", () => {
-    counter -= ghostPrice;
-    ghostPrice *= 1.15;
+    counter -= availableItems[0].price;
+    availableItems[0].price *= availableItems[0].rate;
     ghostCounter++;
-    ghostLabel.innerHTML = `1 👻 / ${(ghostPrice / 100).toFixed(2)} 🎃's`;
+    ghostLabel.innerHTML = `1 👻 / ${(availableItems[0].price / 100).toFixed(
+      2,
+    )} 🎃's`;
     ghostButton.innerHTML = `${ghostCounter} 👻's`;
   });
 
   zombieButton.addEventListener("click", () => {
-    ghostCounter -= zombiePrice;
-    zombiePrice *= 1.15;
+    ghostCounter -= availableItems[1].price;
+    availableItems[1].price *= availableItems[1].rate;
     zombieCounter++;
-    zombieLabel.innerHTML = `$ 🧟 / ${zombiePrice.toFixed(2)} 👻's`;
+    zombieLabel.innerHTML = `$ 🧟 / ${availableItems[1].price.toFixed(2)} 👻's`;
     zombieButton.innerHTML = `${zombieCounter} 🧟's`;
   });
 }
